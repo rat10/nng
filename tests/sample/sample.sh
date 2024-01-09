b88168c2413a0af7162e97300e1b10bd5c8cac7d
james anderson <james@dydra.com>
2024-01-09 23:17:48 +0100

    adding a script which works through sample trig-nng documents

--- tests/sample/sample.sh
+++ tests/sample/sample.sh
@@ -4,11 +4,11 @@
 
 set -e
 if [[ -z $STORE_HOST ]]; then source ../define.sh; fi
-TRANSCRIPT_OUTPUT=`basename ${0} .sh`.txt
+TRANSCRIPT_OUTPUT=sample.txt
 truncate -s 0 $TRANSCRIPT_OUTPUT
 
 function curl_import () {
-  tee -a $TRANSCRIPT_OUTPUT \
+  tee -a $TRANSCRIPT_OUTPUT | tee $ECHO_OUTPUT \
   | curl_graph_store_update -H "Content-Type: application/trig" -w "%{http_code}\n" -o  ${ECHO_OUTPUT} \
   | test_post_success
 }
@@ -51,27 +51,66 @@ curl_import <<EOF
 @prefix nng: <http://nested-named-graph.org/> .
 
 :G1 {
-   :G2 {
+    {:G2 |
        :Alice :buys :Car .
-       :G2 nng:domain [ :age 20 ] ;          
+       :THIS nng:domain [ :age 20 ] ;          
            nng:relation [ :payment :Cash ] ; 
            nng:range :Interpretation ,    
                      [ :color :black ].  
-   } :source :Denis ;                        
-     :purpose :JoyRiding .                   
-   :G3 {    
+   } :source :Denis ; :purpose :JoyRiding .
+   {:G3 |   
        [] {                                  
            :Alice :buys :Car .               
            THIS nng:domain [ :age 28 ] .
-       }
-    } :source :Eve .    
-} :todo { :a :b { :d :e :f . } . } .
-:G6 {  {:q :r :s .} {:u :v :w .} { :x :y :z .}  .}
+       } .
+    } :source :Eve .
+} .
+:G6 { {:G7 | :q :r :s .} { :G8 | :u :v :w .} { :G9 | :x :y :z .}  .} .
 EOF
 
 
 echo >> $TRANSCRIPT_OUTPUT
 curl_graph_store_get >> $TRANSCRIPT_OUTPUT
 
+prefix="prefix : <http://example.org/> prefix : <http://example.org/> prefix nng: <http://nested-named-graph.org/> "
+for document in \
+            "     :a :s :d . " \
+            "   { :a :s :d . } . " \
+            "     :a :s :d ; :s2 :d2 , :d3 . " \
+            "   { :a :s :d ; :s2 :d2 , :d3 . } ." \
+            ":g { :a :s :d . } ." \
+            ":g { :a :s :d ; :s2 :d2 , :d3 . } ." \
+            "       { :x :y :z . } :p :o ." \
+            "{ { :n | :x :y :z . } :p :o . } ." \
+            " << :n | :x :y :z . >> :p :o ." \
+            " << :n | :x :y :z . >> :p :o , :o2." \
+            " << [] | :x :y :z . >> :p :o ; :p2 :o2 ." \
+            "   { [:n nng:Assertion] | :x :y :z . } :p :o ." \
+            "   { [:n nng:Quote] | :x :y :z . } :p :o ." \
+            "   { [:n nng:Record] | :x :y :z . } :p :o ." \
+            "   { [:n nng:Report] | :x :y :z . } :p :o ." \
+            "   :s :p { :x :y :z . } ." \
+            "   :s :p << :x :y :z . >> ." \
+            "   :s :p { :n | :x :y :z . } ." \
+            "   {  :a :s :d . } :p { :x :y :z . } ." \
+            "   { :sg | :a :s :d . } :y { :og | :s :p :o . } ." \
+            ":g {  {:a :s :d . } :y { :s :p :o . } . } ." \
+            "   << :a :s :d . >> :y { :s :p :o . } ." \
+            "   << :a :s :d . >> :y << :s :p :o . >> ." \
+            ":g { << :a :s :d . >> :y << :s :p :o . >> . } ." \
+            "   << :a :s :d . >> :y << :s :p :o . >>  ." \
+            "   << :a :s :d . >> :s << :s :p { :x :y :z . } . >>  ." \
+            ":g {  :a :s :d . } . :g {  :s :p :o . } . {  :x :y :z . } ." \
+            "   {:sg |  :a :s :d . }  {:pg | :s :p :o . } { :og | :x :y :z . } ."
+ do
+   echo >> $TRANSCRIPT_OUTPUT ;
+   curl_clear_repository_content;
+   echo >> $TRANSCRIPT_OUTPUT ;
+   ( echo $prefix ; echo $document ) | curl_import ;
+   echo >> $TRANSCRIPT_OUTPUT ;
+   curl_graph_store_get >> $TRANSCRIPT_OUTPUT ;
+ done
 
  
+
+
